1d58c7184aa60eb5546d4d74d5cccd8601d44e3a
Rui Rua <rui.rrua@gmail.com>
2024-03-21 15:08:00 +0000

    first commit

---
+++ app/src/main/java/com/tds/sampleapp2/CustomEditTextView.java
@@ -0,0 +1,101 @@
+package com.tds.sampleapp2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CustomEditTextView extends androidx.appcompat.widget.AppCompatEditText {
+
+    public String expectedAnswer = "Marega";
+    private boolean isWrong = false;
+    private int color_wrong = Color.RED;
+    private int color_neutral = Color.BLACK;
+
+
+    public CustomEditTextView(@NonNull Context context) {
+        super(context);
+        this.expectedAnswer = "";
+    }
+
+    public CustomEditTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.expectedAnswer = "";
+    }
+
+    public CustomEditTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.expectedAnswer = "";
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+       /* if (isWrong){
+            Paint p = new Paint();
+            p.setColor(color_wrong);
+            p.setStrokeWidth(100);
+            canvas.drawLine(0, this.getY(), this.getWidth(), this.getHeight(), p);
+            canvas.drawLine(0, this.getHeight(), this.getWidth(), this.getY(), p);
+        }*/
+        super.onDraw(canvas);
+        if (isWrong){
+        }
+    }
+
+    public boolean isWrong() {
+        return isWrong;
+    }
+
+    public void setWrong(boolean wrong) {
+        isWrong = wrong;
+        if (isWrong){
+            GradientDrawable border = new GradientDrawable();
+            border.setColor(Color.BLACK);  // Background color
+            border.setStroke(1, Color.RED);  // Border color and width
+            // Set corner radius if needed
+            // border.setCornerRadius(8);
+
+            // Set the custom background drawable to the EditText
+            this.setBackground(border);
+        }
+        else{
+            GradientDrawable border = new GradientDrawable();
+            border.setColor(Color.BLACK);  // Background color
+            border.setStroke(1, Color.GREEN);  // Border color and width
+            // Set corner radius if needed
+            // border.setCornerRadius(8);
+            expectedAnswer = this.getText() != null? this.getText().toString() : "";
+            System.out.println("batatinha");
+            // Set the custom background drawable to the EditText
+            this.setBackground(border);
+        }
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        if (expectedAnswer == null){
+            return;
+        }
+        if (!expectedAnswer.equalsIgnoreCase(text != null ? text.toString() : "")){
+            this.setBorderColor(this.color_neutral);
+            return;
+        }
+        this.setBorderColor(this.color_wrong);
+    }
+
+    private void setBorderColor(int color) {
+        GradientDrawable border = new GradientDrawable();
+        border.setColor(Color.BLACK);
+        border.setStroke(1, color);
+        this.setBackground(border);
+    }
+
+}
---
+++ app/src/main/java/com/tds/sampleapp2/FirstFragment.java
@@ -0,0 +1,55 @@
+package com.tds.sampleapp2;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.tds.sampleapp2.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonFirst.setOnClickListener(view1 -> {
+            String text = "errado";
+            String answer = binding.et.getText() != null ? binding.et.getText().toString() : "";
+            if (!answer.equalsIgnoreCase("marega")){
+                binding.et.setWrong(true);
+            }
+            else{
+                text =  "acertou";
+                binding.et.setWrong(false);
+            }
+            Toast t =  Toast.makeText(binding.getRoot().getContext(), text, Toast.LENGTH_LONG);
+            t.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 100);
+            t.show();
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
